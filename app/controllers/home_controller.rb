6fff4d46a6aa52f339422202ca3a6be0452baee9
francoraniolo <franco11-09@hotmail.com>
2024-02-18 18:43:15 -0300

    Setup optional periodically updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @btc_rate = fetch_btc_rate
-    respond_to do |format|
-      format.html { render }
-      format.turbo_stream  { render turbo_stream: turbo_stream.replace('btc-rate', partial: 'btc_rate', locals: { btc_rate: @btc_rate }) }
-    end
   end
 
   def fetch_btc_rate
